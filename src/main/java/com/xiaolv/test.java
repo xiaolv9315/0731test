0073e86f605bb42e7cc5c2b35eedf3f824b284ba
xiaolv <2450129657@qq.com>
2022-07-31 15:50:43 +0800

    third

--- src/main/java/com/xiaolv/test.java
+++ src/main/java/com/xiaolv/test.java
@@ -4,5 +4,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("2");
+        System.out.println("3");
+        System.out.println("branch1");
     }
 }
